8a2d45122f8d63c5d14a18305c8e40724b8b60e6
Marcin Woźniak <marcinwozniak407@gmail.com>
2023-08-11 20:17:29 +0200

    Implement Dio get method

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
+    //http://api.weatherapi.com/v1/current.json?key=f9fae6b323f64be2923122136230508&q=Warsaw&aqi=no
+    final response = await Dio().get(
+        'http://api.weatherapi.com/v1/current.json?key=f9fae6b323f64be2923122136230508&q=Warsaw&aqi=no');
+    print(response.data);
     return const WeatherModel(city: 'Warsaw', temperature: -5.5);
   }
 }
